c458b90f6fb3fd5a04c346e2311dfa9900cf51a0
Neipee <galaad.goutier@gmail.com>
2026-03-22 20:01:41 +0100

    fix : use plain instead of sha 256 cause MAL don't handle it

--- src/background.js
+++ src/background.js
@@ -1,33 +1,64 @@
-importScripts("config.js")
+importScripts("config.js");
 console.log("background loaded 1");
 let listen_one_time = true;
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (tab.url && tab.url.startsWith("https://myanimelist.net/?code=")&& listen_one_time) {
-    listen_one_time =false;
+  if (
+    tab.url &&
+    tab.url.startsWith("https://myanimelist.net/?code=") &&
+    listen_one_time
+  ) {
+    listen_one_time = false;
     const urlObj = new URL(tab.url);
     const code = urlObj.searchParams.get("code");
     console.log(code);
 
-    chrome.storage.local
-      .get("code_verifier", function (result) {
-        console.log("verifier:", result.code_verifier)
-        const Code_Verifier = result.code_verifier;
-        fetch("https://myanimelist.net/v1/oauth2/token", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: new URLSearchParams({
-            client_id: CLIENT_ID,
-            client_secret: CLIENT_SECRET,
-            grant_type: "authorization_code",
-            code: code,
-            redirect_uri: "https://myanimelist.net",
-            code_verifier: Code_Verifier,
-          }),
-        })  .then((res) => res.json())
-      .then((data) => console.log(data));
-      });
-    
+    chrome.storage.local.get("code_verifier", function (result) {
+      const Code_Verifier = result.code_verifier;
+      fetch("https://myanimelist.net/v1/oauth2/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          client_id: CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+          grant_type: "authorization_code",
+          code: code,
+          redirect_uri: "https://myanimelist.net/",
+          code_verifier: Code_Verifier,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          chrome.storage.local.set({
+            access_token: data.access_token,
+            refresh_token: data.refresh_token,
+          });
+        });
+    });
   }
+});
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  chrome.storage.local.get("access_token", function (result) {
+    const token = result.access_token;
+
+    console.log("message envoyé recu a back ", message.Message);
+    if (message.action === "searchAnime") {
+    fetch(
+      "https://api.myanimelist.net/v2/anime?q=" + message.Message + "&limit=1",
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      },
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        sendResponse({ animeId: data.data[0].node.id });
+      });
+    }
+  });
 });
--- src/popup.js
+++ src/popup.js
@@ -1,26 +1,19 @@
 document.getElementById("login-btn").addEventListener("click", function () {
   console.log("bouton cliqué !");
   const verifier = generateCodeVerifier();
-  generateCodeChallenge(verifier).then((challenge) => {
-    chrome.storage.local.set({
-      code_verifier: verifier,
-    });
-    chrome.tabs.create({ url: buildAuthUrl(challenge) });
+  chrome.storage.local.set({code_verifier : verifier}, function(){
+  chrome.tabs.create({ url: buildAuthUrl(verifier) });
   });
 });
 
 function generateCodeVerifier() {
-  const rdm_values = String.fromCharCode(
-    ...crypto.getRandomValues(new Uint8Array(64)),
-  );
-  const convert = btoa(rdm_values)
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_")
-    .replace(/=+$/, "");
-  return convert;
+  const array = crypto.getRandomValues(new Uint8Array(32));
+  return Array.from(array, (byte) =>
+    ("0" + (byte & 0xff).toString(16)).slice(-2),
+  ).join("");
 }
 
-async function generateCodeChallenge(verifier) {
+/*async function generateCodeChallenge(verifier) {
   const encoder = new TextEncoder();
   const data = encoder.encode(verifier);
   const SHA = await window.crypto.subtle.digest("SHA-256", data);
@@ -31,16 +24,16 @@ async function generateCodeChallenge(verifier) {
     .replace(/\//g, "_")
     .replace(/=+$/, "");
   return convert;
-}
+}*/
 
-function buildAuthUrl(challenge) {
+function buildAuthUrl(verifier) {
   const url =
     "https://myanimelist.net/v1/oauth2/authorize?response_type=code" +
     "&client_id=" +
     CLIENT_ID +
-    "&redirect_uri=https://myanimelist.net" +
+    "&redirect_uri=https://myanimelist.net/" +
     "&code_challenge=" +
-    challenge +
-    "&code_challenge_method=S256";
+    verifier +
+    "&code_challenge_method=plain";
   return url;
 }
