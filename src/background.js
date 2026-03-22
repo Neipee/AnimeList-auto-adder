e543f4d012a56b5a81771696b4980dedcf6ceb79
Neipee <galaad.goutier@gmail.com>
2026-03-22 21:37:43 +0100

    feat: almost finished fined out that other google extension do that but better

--- src/background.js
+++ src/background.js
@@ -2,6 +2,60 @@ importScripts("config.js");
 console.log("background loaded 1");
 let listen_one_time = true;
 
+
+function refreshAccessToken() {
+  chrome.storage.local.get("refresh_token", function (result) {
+    if (!result.refresh_token) return;
+
+    fetch("https://myanimelist.net/v1/oauth2/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        grant_type: "refresh_token",
+        refresh_token: result.refresh_token,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.access_token) {
+          chrome.storage.local.set({
+            access_token: data.access_token,
+            refresh_token: data.refresh_token,
+          });
+          console.log("refreshed tokenn");
+        }
+      });
+  });
+}
+
+function fetchWithAuth(url, options, sendResponse) {
+  chrome.storage.local.get("access_token", function (result) {
+    const token = result.access_token;
+    options.headers = {
+      ...options.headers,
+      Authorization: "Bearer " + token,
+    };
+
+    fetch(url, options)
+      .then((res) => {
+        if (res.status === 401) {
+
+          refreshAccessToken();
+          setTimeout(() => fetchWithAuth(url, options, sendResponse), 1000);
+          return null;
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data) sendResponse({ anime: data });
+      });
+  });
+}
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (
     tab.url &&
@@ -11,10 +65,8 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     listen_one_time = false;
     const urlObj = new URL(tab.url);
     const code = urlObj.searchParams.get("code");
-    console.log(code);
 
     chrome.storage.local.get("code_verifier", function (result) {
-      const Code_Verifier = result.code_verifier;
       fetch("https://myanimelist.net/v1/oauth2/token", {
         method: "POST",
         headers: {
@@ -26,39 +78,57 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
           grant_type: "authorization_code",
           code: code,
           redirect_uri: "https://myanimelist.net/",
-          code_verifier: Code_Verifier,
+          code_verifier: result.code_verifier,
         }),
       })
         .then((res) => res.json())
         .then((data) => {
-          console.log(data);
           chrome.storage.local.set({
             access_token: data.access_token,
             refresh_token: data.refresh_token,
           });
+          console.log("connecté !");
         });
     });
   }
 });
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  chrome.storage.local.get("access_token", function (result) {
-    const token = result.access_token;
+  if (message.action === "searchAnime") {
+    console.log(message.animeName, "episode:", message.episode);
 
-    console.log("message envoyé recu a back ", message.Message);
-    if (message.action === "searchAnime") {
-    fetch(
-      "https://api.myanimelist.net/v2/anime?q=" + message.Message + "&limit=1",
-      {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      },
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        sendResponse({ animeId: data.data[0].node.id });
-      });
-    }
-  });
+    chrome.storage.local.get("access_token", function (result) {
+      const token = result.access_token;
+
+
+      fetch(
+        "https://api.myanimelist.net/v2/anime?q=" +
+          message.animeName +
+          "&limit=1",
+        { headers: { Authorization: "Bearer " + token } },
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          const animeId = data.data[0].node.id;
+
+          fetchWithAuth(
+            "https://api.myanimelist.net/v2/anime/" +
+              animeId +
+              "/my_list_status",
+            {
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+              },
+              body: new URLSearchParams({
+                status: message.status,
+                num_watched_episodes: message.episode,
+              }),
+            },
+            sendResponse,
+          );
+        });
+    });
+    return true;
+  }
 });
--- src/content.js
+++ src/content.js
@@ -1,2 +1,47 @@
-// background
-console.log("content loaded")
+console.log("content.js loaded");
+
+const observer = new MutationObserver(function (mutations) {
+  const title = document.getElementById("titreOeuvre");
+
+  if (title) {
+    const title_alone = title.innerHTML;
+    console.log("titre:", title_alone);
+document.getElementById("nextEpisode").addEventListener("click", function() {
+
+  setTimeout(function() {
+    const episode = document.getElementById("selectEpisodes").value
+    sendAnimeToMAL(episode)
+  }, 500)
+})
+    function sendAnimeToMAL(episodeValue) {
+  const select = document.getElementById("selectEpisodes")
+  const lastEpisode = select.options[select.options.length - 1].value
+  const episode_n = episodeValue.replace(/^\D+/g, "")
+  const lastEpisode_n = lastEpisode.replace(/^\D+/g, "")
+  
+  const status = episode_n === lastEpisode_n ? "completed" : "watching"
+  console.log("envoi épisode:", episode_n, "statut:", status)
+
+  chrome.runtime.sendMessage({
+    action: "searchAnime",
+    animeName: title_alone,
+    episode: episode_n,
+    status: status,
+  }, function(response) {
+    console.log(response.anime)
+  })
+}
+
+
+    const episodeInitial = document.getElementById("selectEpisodes").value;
+    sendAnimeToMAL(episodeInitial);
+
+    document.getElementById("selectEpisodes").addEventListener("change", function () {
+      sendAnimeToMAL(this.value);
+    });
+
+    observer.disconnect();
+  }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
