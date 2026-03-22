bacc90a058f26b9b4913940201d030899bb72235
Neipee <galaad.goutier@gmail.com>
2026-03-22 03:35:53 +0100

    feat: popup.js, and fixing visual issue with popup.html

---
+++ src/popup.js
@@ -0,0 +1,44 @@
+document.getElementById("login-btn").addEventListener("click", function () {
+      console.log("bouton cliqué !");
+    const verifier = generateCodeVerifier();
+    chrome.storage.local.set({ code_verifier: verifier })
+    generateCodeChallenge(verifier).then((challenge) => {
+  chrome.tabs.create({ url: buildAuthUrl(challenge) })
+});
+
+});
+
+function generateCodeVerifier() {
+  const rdm_values = String.fromCharCode(
+    ...crypto.getRandomValues(new Uint8Array(64)),
+  );
+  const convert = btoa(rdm_values)
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+  return convert;
+}
+
+async function generateCodeChallenge(verifier) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(verifier);
+  const SHA = await window.crypto.subtle.digest("SHA-256", data);
+  const hashArray = Array.from(new Uint8Array(SHA));
+  const hashText = String.fromCharCode(...hashArray);
+  const convert = btoa(hashText)
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+  return convert;
+}
+
+function buildAuthUrl(challenge) {
+  const url =
+    "https://myanimelist.net/v1/oauth2/authorize?response_type=code"+
+    "&client_id="+CLIENT_ID+
+    "&redirect_uri=https://myanimelist.net"+
+    "&code_challenge="+
+    challenge+
+    "&code_challenge_method=S256";
+  return url;
+}
